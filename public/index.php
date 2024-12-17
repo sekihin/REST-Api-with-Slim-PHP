db60349a7045730cbe24f950008813abc7b8ae76
sekihin <sekihin@sina.cn>
2024-12-17 13:55:18 +0800

    skeleton

--- public/index.php
+++ public/index.php
@@ -1,23 +1,7 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Selective\BasePath\BasePathMiddleware;
-use Slim\Factory\AppFactory;
+declare(strict_types=1);
 
-require '../vendor/autoload.php';
-
-$app = new \Slim\App();
-
-$app = AppFactory::create();
-
-$app->addRoutingMiddleware();
-$app->add(new BasePathMiddleware($app));
-$app->addErrorMiddleware(true, true, true);
-
-// Import routes
-require '../src/routes/hello-api.php';
+require __DIR__ . '/../src/App/App.php';
 
 $app->run();
-
-?>
--- src/routes/hello-api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    $data = array('message' => 'Hello, ' . $args['name']);
-    return $response->withJson($data);
-});
-
-?>
